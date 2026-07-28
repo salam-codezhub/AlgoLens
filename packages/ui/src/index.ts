9857f0505a7e36e41e7bafd4d77ed5e523f65917
Salam Khan <salamofficial2008@gmail.com>
2026-07-29 01:11:14 +0530

    Add files via upload

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,11 @@
+export * from "./page-placeholder.js";
+export * from "./lib/utils.js";
+export * from "./components/button.js";
+export * from "./components/card.js";
+export * from "./components/dialog.js";
+export * from "./components/input.js";
+export * from "./components/tabs.js";
+export * from "./components/accordion.js";
+
+/** Canonical package name, useful for logging and diagnostics. */
+export const PACKAGE_NAME = "@algolens/ui" as const;
